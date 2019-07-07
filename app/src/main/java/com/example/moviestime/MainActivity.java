5dd9bd688739b40ee9b0083a0766a98e6f1da02c
burhan52 <burhan.r52@gmail.com>
2019-07-07 19:07:42 +0530

    nwe

--- app/src/main/java/com/example/moviestime/MainActivity.java
+++ app/src/main/java/com/example/moviestime/MainActivity.java
@@ -1,36 +1,117 @@
 package com.example.moviestime;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.DownloadListener;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    public WebView mywebview;
+import im.delight.android.webview.AdvancedWebView;
+
+public class MainActivity extends AppCompatActivity implements AdvancedWebView.Listener {
+
+    public static final String TAG = "MainActivity";
+
+    public AdvancedWebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mywebview=(WebView)findViewById(R.id.webview);
-        WebSettings webSettings=mywebview.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        mywebview.loadUrl("https://timemoviesworld.blogspot.com/");
+        mWebView = (AdvancedWebView) findViewById(R.id.webview);
+        mWebView.setListener(this, this);
 
-        mywebview.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl("https://timemoviesworld.blogspot.com/");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mWebView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mWebView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mWebView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mWebView.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
     public void onBackPressed() {
-        if (mywebview.canGoBack())
-        {
-            mywebview.goBack();
+        if (!mWebView.onBackPressed()) {
+            return;
         }
-        else {
-            super.onBackPressed();
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onPageStarted(String url, Bitmap favicon) {
+        Log.d(TAG, "onPageStarted() called with: url = [" + url + "], favicon = [" + favicon + "]");
+    }
+
+    @Override
+    public void onPageFinished(String url) {
+        Log.d(TAG, "onPageFinished() called with: url = [" + url + "]");
+    }
+
+    @Override
+    public void onPageError(int errorCode, String description, String failingUrl) {
+        Log.d(TAG, "onPageError() called with: errorCode = [" + errorCode + "], description = [" + description + "], failingUrl = [" + failingUrl + "]");
+    }
+
+    @Override
+    public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength, String contentDisposition, String userAgent) {
+        Log.d(TAG, "onDownloadRequested() called with: url = [" + url + "], suggestedFilename = [" + suggestedFilename + "], mimeType = [" + mimeType + "], contentLength = [" + contentLength + "], contentDisposition = [" + contentDisposition + "], userAgent = [" + userAgent + "]");
+        try {
+            Uri outputUri =  Uri.parse(url);
+            Intent intent = new Intent(Intent.ACTION_VIEW, outputUri);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            List<ResolveInfo> resInfoList = getPackageManager()
+                    .queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+            if (resInfoList == null) {
+                resInfoList = new ArrayList<>();
+            }
+            for (ResolveInfo resolveInfo : resInfoList) {
+                String packageName = resolveInfo.activityInfo.packageName;
+                grantUriPermission(packageName, outputUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            }
+            startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public void onExternalPageRequest(String url) {
+        Log.d(TAG, "onExternalPageRequest() called with: url = [" + url + "]");
     }
 }
